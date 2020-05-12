f88b107a120ce4c22a93d5bc52be7ac2225654db
Sherry Zhang <huizezhangsh@gmail.com>
2020-05-12 13:45:06 +1000

    row number start from 0; fix color; fix combined plot with polish

--- R/explore_trace_all.R
+++ R/explore_trace_all.R
@@ -24,6 +24,9 @@ explore_trace_all <- function(glb_obj, magnify = FALSE){
   tries <- rlang::sym("tries")
   loop <- rlang::sym("loop")
 
+  glb_obj <- glb_obj %>%
+    dplyr:: mutate(id = dplyr::row_number()-1)
+
   p <- glb_obj %>%
     ggplot(aes(x = !!id, y = !!index_val, col = !!info)) +
     geom_point()
@@ -37,11 +40,14 @@ explore_trace_all <- function(glb_obj, magnify = FALSE){
     frac <- glb_obj %>%
       dplyr::filter(tries == tries_to_magnify, info != "line_search")
 
+    num_level <- length(levels(as.factor(glb_obj$info)))
+    level_selected <- which(levels(as.factor(glb_obj$info)) %in% levels(as.factor(frac$info)))
+
     p_frac <- frac %>%
       ggplot(aes(x = !!id, y = !!index_val, col = !!info)) +
       geom_point() +
       xlim(1, max(frac$id)) +
-      scale_color_manual(values = scales::hue_pal()(6)[c(2, 3, 5)]) +
+      scale_color_manual(values = scales::hue_pal()(num_level)[level_selected]) +
       theme(legend.position = "none")
 
     p <- p_frac/p +
@@ -57,21 +63,28 @@ explore_trace_all <- function(glb_obj, magnify = FALSE){
 
 #'@export
 #'@rdname explore_trace_all
-explore_trace_interp <- function(glb_obj){
+explore_trace_interp <- function(glb_obj, point = FALSE){
+  #browser()
 
   # check there is a column called info, there is a value called interpolation
   # check other variables as well
   info <- rlang::sym("info")
   id <- rlang::sym("id")
   index_val <- rlang::sym("index_val")
+  method <- rlang::sym("method")
 
   interp <- glb_obj %>%
-    dplyr::filter(!!info == "interpolation") %>%
-    dplyr::mutate(id = dplyr::row_number())
+    dplyr::filter(!!info == "interpolation" | (!!info == "polish_best" & !!method == "search_polish")) %>%
+    dplyr::mutate(id = dplyr::row_number()-1)
 
   p <- interp %>%
-    ggplot(aes(x = !!id, y = !!index_val))  +
+    ggplot(aes(x = !!id, y = !!index_val, group = 1))  +
     geom_line()
+
+  if (point){
+    p <- p + geom_point(aes(col = as.factor(!!method)))
+  }
+
 
   p
 }
