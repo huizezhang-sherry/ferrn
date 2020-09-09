6318a4fd5737af5266aefb6df82966c39beb7b3c
Sherry <huizezhangsh@gmail.com>
2020-09-09 17:36:27 +1000

    helper function to extract the best observation(s)

--- R/get.R
+++ R/get.R
@@ -1,17 +1,37 @@
 #'@title get the basis/ index value
 #'@param dt a data object from the \href{https://cran.r-project.org/web/packages/tourr}{tourr} package
+#'@param group the grouping variable, useful when there are multiple trial of tours in dt
+#'@param var the variable to select if not returning everything
 #'@examples
-#'\dontrun{get_best(holes_1d_better)$basis}
-#'\dontrun{get_best(holes_1d_better)$index_val}
-#'@return a list with the best index value along with its basis
+#'\dontrun{gholes_1d_better %>% get_start()}
+#'\dontrun{bind_rows(holes_1d_better, holes_1d_geo) %>% get_best(group = method, var = c(basis, index_val))}
+#'@return a tibble with the start/best observation(s)
 #'@export
 #'@rdname get_best
-get_best <- function(dt){
+get_best <- function(dt, group = NULL, var = NULL){
 
-  obj <- dt %>%
-    dplyr::filter(info == "interpolation") %>%
-    tail(1)
+  group <- rlang::enexpr(group)
+  var <- rlang::enexprs(var)
 
-  list(basis = obj %>% dplyr::pull(basis) %>% .[[1]],
-       index_val = obj %>% dplyr::pull(index_val))
+  res <- dt %>%
+    dplyr::filter(!!sym("info") == "interpolation") %>%
+    dplyr::group_by(!!group) %>%
+    dplyr::filter(!!sym("id") == max(id))
+
+  if (!is.null(var)){
+    if(!(!!var %in% colnames(dt))){
+      stop("var needs to be one of the variables in the data object!")
+    }
+    res <- res %>% dplyr::select(!!var)
+  }
+
+  res
+}
+
+#'@export
+#'@rdname get_best
+get_start <- function(dt){
+
+  dt %>%
+    dplyr::filter(!!sym("id") == 1)
 }
