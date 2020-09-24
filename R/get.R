2500e199c9ac4e797b1c501a5487104aae171ee9
Sherry <huizezhangsh@gmail.com>
2020-09-24 12:11:18 +1000

    bind theoretical

--- R/get.R
+++ R/get.R
@@ -18,12 +18,12 @@ get_best <- function(dt, group = NULL, var = NULL){
     dplyr::group_by(!!group) %>%
     dplyr::filter(!!sym("id") == max(id))
 
-  if (!is.null(var)){
-    if(!(!!var %in% colnames(dt))){
-      stop("var needs to be one of the variables in the data object!")
-    }
-    res <- res %>% dplyr::select(!!var)
-  }
+  # if (!is.null(var)){
+  #   if(!(!!var %in% colnames(dt))){
+  #     stop("var needs to be one of the variables in the data object!")
+  #   }
+  #   res <- res %>% dplyr::select(!!var)
+  # }
 
   res
 }
@@ -34,4 +34,41 @@ get_start <- function(dt){
 
   dt %>%
     dplyr::filter(!!sym("id") == 1)
+}
+
+
+#' @title bind the theoretical basis to a given data object
+#' @param dt the data object being binded to
+#' @param matrix the theoretical basis to bind
+#' @param index the index function used to calculate index value
+#' @examples
+#' best <- matrix(c(0, 1, 0, 0, 0), nrow = 5)
+#' holes_1d_better %>% bind_theoretical(best, tourr::holes()) %>% tail()
+#' dt <- bind_rows(holes_1d_better, holes_1d_geo)
+#' dt %>%  bind_theoretical(best, tourr::holes()) %>% tail()
+#' @export
+bind_theoretical <- function(dt, matrix, index){
+
+  num_row <- nrow(dt$basis[[1]])
+  num_col <- ncol(dt$basis[[1]])
+
+  if (ncol(matrix) != num_col | nrow(matrix) != num_row){
+    stop("theoretical best matrix need to be of the same dimension as the data object!")
+  }
+
+  method_index <- !is.na(unique(dt$method))
+  method <- unique(dt$method)[method_index]
+
+  theo <- tibble::tibble(basis = list(matrix),
+                         index_val = index(matrix),
+                         tries = NA,
+                         info = "theoretical",
+                         loop = NA,
+                         method = method,
+                         alpha = NA,
+                         id = max(dt$id) + 1)
+
+
+  dt %>% dplyr::bind_rows(theo)
+
 }
